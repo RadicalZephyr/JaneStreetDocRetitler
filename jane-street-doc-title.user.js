51430888c19f3afb0d9bbe4500bfd2d5b96093d8
Geoff Shannon <geoffpshannon@gmail.com>
2014-10-22 16:41:19 -0400

    Initial commit

---
+++ jane-street-doc-title.user.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @name         Jane Street Doc Headers
+// @namespace    http://zephyrizing.net/
+// @version      0.1
+// @description  Put the name of the current library in the page title
+// @author       Geoff Shannon
+// @match        https://ocaml.janestreet.com/ocaml-core/111.28.00/doc/*
+// @grant        none
+// ==/UserScript==
+
+
+// Ids:
+// #bccurpkg
+// document.getElementById("bccurpkg").firstChild.text
+// #bccurpkgmod
+// document.getElementById("bccurpkgmod").firstChild
